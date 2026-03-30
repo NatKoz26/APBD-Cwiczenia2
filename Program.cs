2d3139ba4f682152d5ad7b864e6cccca97519c07
NatKoz26 <s26513@pjwstk.edu.pl>
2026-03-30 22:12:58 +0200

    Wstepna konfiguracja program.cs - pod testy

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        
+        var laptop = new Laptop { Id = 1, Name = "Dell", Ram = 16, Processor = "i7" };
+        var camera = new Camera { Id = 2, Name = "Canon", Resolution = 24, HasFlash = true };
+
+        var student = new Student { Id = 1, FirstName = "Jan", LastName = "Kowalski" };
+        var employee = new Employee { Id = 2, FirstName = "Anna", LastName = "Nowak" };
+
+        Console.WriteLine($"Test: {employee.LastName}, {employee.Id}, {employee.FirstName}");
+        
+    }
+}
